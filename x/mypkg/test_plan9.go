ffd9d35fef4e8f3714b5e3e40dfa4c927762c379
Sean Hinchee <henesy.dev@gmail.com>
2018-02-21 12:14:09 -0600

    tested on 9

--- x/mypkg/test_plan9.go
+++ x/mypkg/test_plan9.go
@@ -10,12 +10,8 @@ import (
 
 func init() {
 	FillSystemInfo = func(sys interface{}) {
-		if os, ok := sys.(*syscall.Stat_t); ok {
-			fmt.Println(time.Unix(int64(os.Ctim.Sec), int64(os.Ctim.Nsec)))
-			fmt.Println(uint32(os.Dev))
-			fmt.Println(uint32(os.Ino))
-			fmt.Println(os.Gid)
-			fmt.Println(os.Uid)
+		if os, ok := sys.(*syscall.Dir); ok {
+			fmt.Println(os)
 		}
 	}
 }
